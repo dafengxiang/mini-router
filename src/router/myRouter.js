09bcca28447d37f7024257a939c66c1baa059f1c
dafengxiang <1205679862@qq.com>
2023-03-07 17:29:43 +0800

    feat: 自定义router文件，替换引用

---
+++ src/router/myRouter.js
@@ -0,0 +1,12 @@
+/*
+ * @Description: 自定义router
+ * @Author: wangfengxiang
+ * @Date: 2023-03-05 08:18:33
+ * @LastEditTime: 2023-03-05 08:23:46
+ * @LastEditors: wangfengxiang
+ */
+export default {
+    install(Vue){
+        
+    }
+}
